3f4cf8d43d1cdfb25b6fc1cb91e5400e8fbb7aed
dksviolin <dksviolin@d-gamez.com>
2017-01-24 18:33:23 +0900

    info change
    info change

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var path = require('path');
 var databaseUri = 'mongodb://parse:parse@ds127429.mlab.com:27429/parse-server-test';
 
 var api = new ParseServer({
-    databaseURI: databaseUri || 'mongodb://parse:parse@ds127429.mlab.com:27429/parse-server-test',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'G3Iu1aEHi9hfGjQZYzjmcAez00Fjw9DDXKsZzNgQ',
-  masterKey: process.env.MASTER_KEY || 'nDdkEjbOv5qEme44oi32mjgHP4EH3y6KZYynQTUR', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://parse-server-examplea633.azurewebsites.net/',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || 'myAppKey', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
